dbeb159440e92e7a67efb218884d914db131083a
rahulspoudel <rahulsharmapoudel@gmail.com>
2022-11-16 13:56:36 +0545

    ### Deploying to a local node (start)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className=" border-b p-6">
+        <p className="text-4xl font-bold">NextGen MarketPlace</p>
+        <div className="flex pt-4">
+          <Link href="/">
+            <p className="mr-4 text-pink-500">Home</p>
+          </Link>
+          <Link href="/create-item">
+            <p className="mr-4 text-pink-500">Sell Digital Asset</p>
+          </Link>
+          <Link href="/my-assets">
+            <p className="mr-4 text-pink-500">My Digital Assets</p>
+          </Link>
+          <Link href="/dashboard">
+            <p className="mr-4 text-pink-500">Creator Dashboard</p>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,20 +7,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  const nftMarket = await NFTMarket.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await nftMarket.deployed();
+  console.log("nftMarket deployed to: ", nftMarket.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy(nftMarket.address);
+  await nft.deployed();
+  console.log("nft deployted to: ", nft.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
